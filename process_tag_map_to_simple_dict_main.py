34bd28df3de1d95c632a8e1d4782821244e3b6f0
Asher Metzger <ashermetzger@gmail.com>
2024-02-15 18:38:17 +0200

    Removes imports and formatting.

--- process_tag_map_to_simple_dict_main.py
+++ process_tag_map_to_simple_dict_main.py
@@ -1,33 +1,21 @@
-"""Getting revenues.
-"""
+"""Getting revenues."""
 
-import os
-import glob
-import pathlib
 from absl import app
-import time
-import requests
 import json
-import pickle
-import tqdm
-import itertools
 import collections
-
-import pandas as pd
-from absl import logging
 
 
 def main(_):
-    with open('tagmap.json', 'rt') as f:
+    with open("tagmap.json", "rt") as f:
         variables = json.load(f)
-    
+
     tags_by_var = collections.defaultdict(list)
     for var in variables:
-        print(var['tags'])
-        tags = var['tags']
+        print(var["tags"])
+        tags = var["tags"]
         tags_by_var[tags[0]] = tags
-    
-    with open('tags_by_var.json', 'wt') as f:
+
+    with open("tags_by_var.json", "wt") as f:
         json.dump(tags_by_var, f)
 
 
